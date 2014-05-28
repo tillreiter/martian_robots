b311dd4503e20a9a3396bef6a15ce8eb17427ce1
Till Reiter <till.reiter@gmail.com>
2014-05-28 22:52:06 +0200

    add comments

--- martian_robots.js
+++ martian_robots.js
@@ -1,8 +1,14 @@
+// UserInput constructor to check input and clean it up for further processing
 function UserInput(input) {
   this.userInput = this.verify(input);
-}
+  this.roboInstructions = [];
 
+};
+
+// if correct commands are entered pull them out and ignore the rest; wrong input returns false
 UserInput.prototype.verify = function (input){
+
+  // regex to filter out entire command, if new instructions get added to program add to [LRF] in pGridAndCommands and adjust Robot prototype accordingly
   var pGridAndCommands = /(\d+\s+\d+\s*\n+)((\d+\s+\d+\s*[NESW]{1}\s*\n+[LRF]+\s*\n*)+)/i;
 
   var verify = input.match(pGridAndCommands);
@@ -15,6 +21,7 @@ UserInput.prototype.verify = function (input){
   }
 };
 
+// deconstruct verified input
 UserInput.prototype.processInput = function (){
   var pSplitLines = /[\r?\n]+/;
   var pGridDefinition = /\d{1,2}\s+\d{1,2}/;
@@ -24,13 +31,14 @@ UserInput.prototype.processInput = function (){
 
   var lines = splitLines(this.userInput);
 
-  this.roboInstructions = [];
+  // first line is always grid definition and gets shifted from lines var
   this.gridCoordinates = lines.shift().match(pGridDefinition)[0].split(pSplitWhitespace);
 
   function splitLines(string) {
     return string.split(pSplitLines);
   }
 
+  // iterate over raw robot instruction; 2 elements form one instruction
   for (var i = 0; i < lines.length; i+=2) {
     var rawCoordinates = lines[i];
     var rawInstruction = lines [i+1];
@@ -41,6 +49,8 @@ UserInput.prototype.processInput = function (){
   }
 };
 
+// build roboInstructions object and push it to instructions array
+// where should I verify grid not bigger than 50?
 UserInput.prototype.newRoboCommand = function newRoboCommand (x,y,direction,instruction) {
   var command = {
     x: x,
@@ -54,25 +64,31 @@ UserInput.prototype.newRoboCommand = function newRoboCommand (x,y,direction,inst
 // set up Planet grid constructor
 function Planet (gridCoordinates) {
 
+  // add 1 to width and height so smallest possible grid has 4 cells when entered 0,0 in first row
   this.width = parseInt(gridCoordinates[0]) + 1;
   this.height = parseInt(gridCoordinates[1]) + 1;
-  // this.cells = new Array ((height + 1) * (width + 1));
+
+  // create cells array to push sented cells information
   this.cells = new Array (this.height * this.width)
 };
 
+// check if value is scented
 Planet.prototype.valueAt = function (point) {
   return this.cells[point.y * this.width + point.x];
 }
 
+// scent cell
 Planet.prototype.scentAt = function (point) {
   this.cells[point.y * this.width + point.x] = "scent";
 }
 
+// check if cell is inside
 Planet.prototype.isInside = function (point) {
   return point.x >= 0 && point.y >= 0 && point.x < this.width && point.y < this.height;
 }
 
 
+// Robot constructor to build single robots
 function Robot (x, y, direction, instruction) {
   directions = ["N", "E", "S", "W"];
 
@@ -85,59 +101,27 @@ function Robot (x, y, direction, instruction) {
   this.lost = false;
 }
 
-Robot.prototype.turnLeft = function (direction) {
-  var currentDir = directions.indexOf(direction);
-  if(currentDir === 0){currentDir = 4};
-  return directions[currentDir - 1];
-}
-
-Robot.prototype.turnRight = function (direction) {
-  var currentDir = directions.indexOf(direction);
-  if(currentDir === 3){currentDir = -1};
-  return directions[currentDir + 1];
-}
-
-Robot.prototype.moveForward = function () {
-  var oldPosition = {x: this.position.x, y: this.position.y};
-  var newPosition = {x: this.position.x, y: this.position.y};
-
-  if (this.direction === "N") {this.position.y ++};
-  if (this.direction === "E") {this.position.x ++};
-  if (this.direction === "S") {this.position.y --};
-  if (this.direction === "W") {this.position.x --};
-
-  if (mars.isInside(this.position) === false) {
-    console.log("lets hope");
-    if (mars.valueAt(oldPosition) === "scent") {
-      this.position = oldPosition;
-      console.log("we didnt go")
-    } else {
-      mars.scentAt(oldPosition);
-      console.log("we scented", oldPosition)
-      this.position = oldPosition;
-      this.lost = true;
-    }
-  }
-}
-
+// execute instruction string
 Robot.prototype.move = function () {
 
-  //if initial robot position is not in grid it gets ignored
+  // if initial robot position is not in grid it gets ignored
   if (mars.isInside(this.position)) {
     for (var i = 0; i < this.instruction.length; i++) {
 
-      // take into account yet defined commands
+      // take into account yet defined instruction - add more instructions here
       if (this.instruction.charAt(i) === 'L') {this.direction = (this.turnLeft(this.direction))};
       if (this.instruction.charAt(i) === 'R') {this.direction = (this.turnRight(this.direction))};
       if (this.instruction.charAt(i) === 'F') {
         this.moveForward();
+
+        // break out of iterating when robot is lost
         if (this.lost === true) {
           break;
         }
       };
     };
 
-    // check if robot wants to move off grid, stop if already scented, otherwise scent
+    // push result of move operation to output
     if (this.lost === true) {
       output.push(this.position.x + " " + this.position.y + " " + this.direction + " " + "LOST")
     } else {
@@ -146,9 +130,51 @@ Robot.prototype.move = function () {
   };
 };
 
-// set up buttons and define functions to run on click
+// switch to before element in directions array
+Robot.prototype.turnLeft = function (direction) {
+  var currentDir = directions.indexOf(direction);
+  if(currentDir === 0){currentDir = 4};
+  return directions[currentDir - 1];
+}
+
+// switch to next element in directions array
+Robot.prototype.turnRight = function (direction) {
+  var currentDir = directions.indexOf(direction);
+  if(currentDir === 3){currentDir = -1};
+  return directions[currentDir + 1];
+}
+
+// move robot one cell in current direction
+Robot.prototype.moveForward = function () {
+  var oldPosition = {x: this.position.x, y: this.position.y};
+
+  if (this.direction === "N") {this.position.y ++};
+  if (this.direction === "E") {this.position.x ++};
+  if (this.direction === "S") {this.position.y --};
+  if (this.direction === "W") {this.position.x --};
+
+  // check if new position is within grid
+  if (mars.isInside(this.position) === false) {
+
+    // if not and oldPosition scent ignore
+    if (mars.valueAt(oldPosition) === "scent") {
+      this.position = oldPosition;
+    }
+
+    // if yes scent cell and mark lost
+    else {
+      mars.scentAt(oldPosition);
+      this.position = oldPosition;
+      this.lost = true;
+    }
+  }
+}
+
+
+// initiate DOM with button functions
 initiate = function () {
 
+  // iterate over robot instructions
   function startRobots (commands) {
     output = [];
     for (var i = 0; i < commands.length; i++) {
@@ -162,12 +188,16 @@ initiate = function () {
   goClick.onclick = function run(){
     var input = document.getElementById("instructions").value;
     var command = new UserInput(input);
+
     if (!command.userInput) {
       alert("this input seems lousy! Did you enter it in the correct format?")
     } else {
-      command.processInput()
-      console.log("command is",command);
+      command.processInput();
+
+      // lay out current grid
       mars = new Planet (command.gridCoordinates);
+
+      // process all user instructions
       startRobots(command.roboInstructions);
       document.getElementById("output").value = output.join("\n");
     }
@@ -178,7 +208,7 @@ initiate = function () {
     alert('Hello')
   };
 
-
+  // reset (global) variables
   var clearClick = document.getElementById("clear");
   clearClick.onclick = function (){
     document.getElementById("instructions").value = "";
